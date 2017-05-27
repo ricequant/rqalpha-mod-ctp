97010c776f41a3a195711e5251341eaff48be090
oscarcui <cuizi7@163.com>
2017-05-27 10:39:17 +0800

    remove useless file

--- rqalpha_mod_ctp/ctp/trade_gateway.py
+++ rqalpha_mod_ctp/ctp/trade_gateway.py
@@ -15,20 +15,22 @@
 # limitations under the License.
 
 from time import sleep
-from six import iteritems
+from six import iteritems, itervalues
 from datetime import date
 
 from rqalpha.utils.logger import system_log
-from rqalpha.const import ACCOUNT_TYPE, ORDER_STATUS
+from rqalpha.const import ACCOUNT_TYPE, ORDER_STATUS,  SIDE, POSITION_EFFECT
 from rqalpha.environment import Environment
 from rqalpha.events import EVENT
 from rqalpha.events import Event as RqEvent
+from rqalpha.model.order import Order
 from rqalpha.model.trade import Trade
 from rqalpha.model.portfolio import Portfolio
+from rqalpha.model.base_position import Positions
 
 from .api import CtpTdApi
-from .data_cache import DataCache
-from ..utils import cal_commission
+from .data_dict import FakeTickDict
+from ..utils import cal_commission, margin_of
 
 
 class TradeGateway(object):
@@ -254,3 +256,146 @@ class TradeGateway(object):
     @staticmethod
     def on_err(error):
         system_log.error('CTP 错误，错误代码：%s，错误信息：%s' % (str(error.ErrorID), error.ErrorMsg.decode('GBK')))
+
+
+class DataCache(object):
+    def __init__(self):
+        self.ins = {}
+        self.future_info = {}
+
+        self.orders = {}
+        self.open_orders = []
+        self.trades = {}
+
+        self.pos = {}
+        self.snapshot = {}
+
+        self._account_dict = None
+        self._qry_order_cache = {}
+        self._account_model = None
+        self._position_model = None
+
+    def cache_ins(self, ins_cache):
+        self.ins = ins_cache
+        self.future_info = {ins_dict.underlying_symbol: {'speculation': {
+                'long_margin_ratio': ins_dict.long_margin_ratio,
+                'short_margin_ratio': ins_dict.short_margin_ratio,
+                'margin_type': ins_dict.margin_type,
+            }} for ins_dict in self.ins.values()}
+
+    def cache_commission(self, underlying_symbol, commission_dict):
+        self.future_info[underlying_symbol]['speculation'].update({
+            'open_commission_ratio': commission_dict.open_ratio,
+            'close_commission_ratio': commission_dict.close_ratio,
+            'close_commission_today_ratio': commission_dict.close_today_ratio,
+            'commission_type': commission_dict.commission_type,
+        })
+
+    def cache_open_order(self, order):
+        if order not in self.open_orders:
+            self.open_orders.append(order)
+
+    def remove_open_order(self, order):
+        if order in self.open_orders:
+            self.open_orders.remove(order)
+
+    def cache_position(self, pos_cache):
+        self.pos = pos_cache
+        for order_book_id, pos_dict in iteritems(pos_cache):
+            if order_book_id not in self.snapshot:
+                self.snapshot[order_book_id] = FakeTickDict(pos_dict)
+
+    def cache_account(self, account_dict):
+        self._account_dict = account_dict
+
+    def cache_qry_order(self, order_cache):
+        self._qry_order_cache = order_cache
+
+    def cache_trade(self, trade_dict):
+        if trade_dict.order_book_id not in self.trades:
+            self.trades[trade_dict.order_book_id] = []
+        self.trades[trade_dict.order_book_id].append(trade_dict)
+
+    def get_cached_order(self, obj):
+        try:
+            order = self.orders[obj.order_id]
+        except KeyError:
+            order = Order.__from_create__(obj.order_book_id, obj.quantity, obj.side, obj.style, obj.position_effect)
+            self.cache_order(order)
+        return order
+
+    def cache_order(self, order):
+        self.orders[order.order_id] = order
+
+    @property
+    def positions(self):
+        PositionModel = self._position_model
+        ps = Positions(PositionModel)
+        for order_book_id, pos_dict in iteritems(self.pos):
+            position = PositionModel(order_book_id)
+
+            position._buy_old_holding_list = [(pos_dict.prev_settle_price, pos_dict.buy_old_quantity)]
+            position._sell_old_holding_list = [(pos_dict.prev_settle_price, pos_dict.sell_old_quantity)]
+
+            position._buy_transaction_cost = pos_dict.buy_transaction_cost
+            position._sell_transaction_cost = pos_dict.sell_transaction_cost
+            position._buy_realized_pnl = pos_dict.buy_realized_pnl
+            position._sell_realized_pnl = pos_dict.sell_realized_pnl
+
+            position._buy_avg_open_price = pos_dict.buy_avg_open_price
+            position._sell_avg_open_price = pos_dict.sell_avg_open_price
+
+            if order_book_id in self.trades:
+                trades = sorted(self.trades[order_book_id], key=lambda t: t.trade_id, reverse=True)
+
+                buy_today_holding_list = []
+                sell_today_holding_list = []
+                for trade_dict in trades:
+                    if trade_dict.side == SIDE.BUY and trade_dict.position_effect == POSITION_EFFECT.OPEN:
+                        buy_today_holding_list.append((trade_dict.price, trade_dict.quantity))
+                    elif trade_dict.side == SIDE.SELL and trade_dict.position_effect == POSITION_EFFECT.OPEN:
+                        sell_today_holding_list.append((trade_dict.price, trade_dict.quantity))
+
+                self.process_today_holding_list(pos_dict.buy_today_quantity, buy_today_holding_list)
+                self.process_today_holding_list(pos_dict.sell_today_quantity, sell_today_holding_list)
+
+                position._buy_today_holding_list = buy_today_holding_list
+                position._sell_today_holding_list = sell_today_holding_list
+
+            ps[order_book_id] = position
+        return ps
+
+    def process_today_holding_list(self, today_quantity, holding_list):
+        # check if list is empty
+        if not holding_list:
+            return
+        cum_quantity = sum(quantity for price, quantity in holding_list)
+        left_quantity = cum_quantity - today_quantity
+        while left_quantity > 0:
+            oldest_price, oldest_quantity = holding_list.pop()
+            if oldest_quantity > left_quantity:
+                consumed_quantity = left_quantity
+                holding_list.append((oldest_price, oldest_quantity - left_quantity))
+            else:
+                consumed_quantity = oldest_quantity
+            left_quantity -= consumed_quantity
+
+    @property
+    def account(self):
+        static_value = self._account_dict.yesterday_portfolio_value
+        ps = self.positions
+        realized_pnl = sum(position.realized_pnl for position in itervalues(ps))
+        cost = sum(position.transaction_cost for position in itervalues(ps))
+        margin = sum(position.margin for position in itervalues(ps))
+        total_cash = static_value + realized_pnl - cost - margin
+
+        AccountModel = self._account_model
+        account = AccountModel(total_cash, ps)
+        account._frozen_cash = sum(
+            [margin_of(order_dict.order_book_id, order_dict.unfilled_quantity, order_dict.price) for order_dict in
+             self._qry_order_cache.values() if order_dict.status == ORDER_STATUS.ACTIVE])
+        return account, static_value
+
+    def set_models(self, account_model, position_model):
+        self._account_model = account_model
+        self._position_model = position_model
